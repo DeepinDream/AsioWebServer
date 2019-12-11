9d2ae031718abfd9d247a5f493df65f1293f8580
root <root@hpr-25004972.combatelecom.com>
2019-12-11 13:34:44 +0800

    添加 Logger 代码

---
+++ test/LogStream_test.cpp
@@ -0,0 +1,21 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "Logger/LogStream.h"
+#include <iostream>
+
+using namespace testing;
+
+TEST(FixedBuffer, test)
+{
+    FixedBuffer<kSmallBuffer> buffer;
+    buffer.append("abc", 3);
+    EXPECT_STREQ(buffer.data(), "abc");
+}
+
+TEST(LogStream, output)
+{
+    LogStream log;
+    log << 123 << "abc";
+    EXPECT_STREQ(log.buffer().data(), "123abc");
+}
